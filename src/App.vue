598675ca5ef1bab64ea35dc439e6b508e2379df3
Fitra Fadilana <fitra90@gmail.com>
2020-02-26 23:41:40 +0700

    adding buefy

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,57 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <b-navbar>
+      <template slot="brand">
+        <b-navbar-item >
+         <b>VueFootballClub</b>
+        </b-navbar-item>
+      </template>
+      <template slot="start">
+        <b-navbar-item href="#">Home</b-navbar-item>
+        <b-navbar-item href="#">Documentation</b-navbar-item>
+        <b-navbar-dropdown label="Info">
+          <b-navbar-item href="#">About</b-navbar-item>
+          <b-navbar-item href="#">Contact</b-navbar-item>
+        </b-navbar-dropdown>
+      </template>
+
+      <template slot="end">
+        <b-navbar-item tag="div">
+          <div class="buttons">
+            <a class="button is-primary">
+              <strong>Sign up</strong>
+            </a>
+            <a class="button is-light">Log in</a>
+          </div>
+        </b-navbar-item>
+      </template>
+    </b-navbar>
+
+    <div class="notification">
+      This container is
+      <strong>centered</strong> on desktop.
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import Vue from "vue";
+import Buefy from "buefy";
+import "buefy/dist/buefy.css";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App"
+  // components: {
+  //   HelloWorld
+  // }
+};
+Vue.use(Buefy, {
+  defaultIconPack: "fas",
+  defaultContainerElement: "#content"
+  // ...
+});
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
